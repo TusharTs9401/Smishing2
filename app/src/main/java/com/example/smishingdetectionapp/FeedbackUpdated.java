3d65d172437a6b6c89e24411faef8d518407e2e6
Tushar Thakkar <karan@stockunlock.com>
2024-12-06 19:03:30 +0530

    Feedback Activity Updated

--- app/src/main/java/com/example/smishingdetectionapp/FeedbackUpdated.java
+++ app/src/main/java/com/example/smishingdetectionapp/FeedbackUpdated.java
@@ -1,4 +1,17 @@
 package com.example.smishingdetectionapp;
 
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View; // Add this import for the View class
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+
 public class FeedbackUpdated {
+
 }
